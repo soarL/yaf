3ca2d59991c24a2440c547c7f4df7ccbe310ab17
soar <271105196@qq.com>
2018-03-12 21:36:55 +0800

    ss

---
+++ application/Bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * 所有在Bootstrap类中, 以_init开头的方法, 都会被Yaf调用,
+ * 这些方法, 都接受一个参数:Yaf_Dispatcher $dispatcher
+ * 调用的次序, 和申明的次序相同
+ */
+use Yaf\Bootstrap_Abstract;
+use Yaf\Application;
+use Yaf\Dispatcher;
+use Yaf\Registry;
+
+
+class Bootstrap extends Bootstrap_Abstract{
+
+        public function _initConfig() {
+                $config = Application::app()->getConfig();
+                Registry::set("config", $config);
+        }
+        public function _initDefaultName(Dispatcher $dispatcher) {
+                $dispatcher->setDefaultModule("Index")->setDefaultController("Index")->setDefaultAction("index");
+        }
+}
+?>
