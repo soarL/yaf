d14490b2c6d9667386f3cd8ffd5de9b303a70715
soar <271105196@qq.com>
2018-03-13 01:11:40 +0800

    ss

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -2,7 +2,7 @@
 use Yaf\Controller_Abstract;
 class IndexController extends Controller_Abstract {
    public function indexAction() {//默认Action
-       $this->getView()->assign("content", "Hello Worssssadasdasasdasdlsssd");
+       $this->getView()->assign("content", "Hello Wolsssd");
    }
 }
 
