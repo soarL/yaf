7ce12f64af3b6aced6237e1a34e665985a7eb87b
lzt <271105196@qq.com>
2018-03-14 14:14:08 +0800

    ss

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -5,15 +5,6 @@ use Illuminate\Database\Capsule\Manager as DB;
 class IndexController extends Controller {
 	public function indexAction() {//默认Action
 		$users = User::where('id','>',1)->orderBy('id','desc')->get();
-		foreach ($users as $key => $value) {
-			echo "<br>";
-			echo $value['id'];
-			echo "<br>";
-			echo $value['username'];
-			echo "<br>";
-			echo $value['password'];
-		};
-		exit;
 		$this->display('index',["users"=>$users]);
 	}
 }
