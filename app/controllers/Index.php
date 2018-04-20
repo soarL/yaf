c8770f90beeae5fee1640190281ca68dbb1349e5
lzt <271105196@qq.com>
2018-04-20 17:26:45 +0800

    ss

--- app/controllers/Index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// use models\User;
-// use Illuminate\Database\Capsule\Manager as DB;
-
-class IndexController extends Controller {
-	public function indexAction() {//默认Action
-		$this->display('index');
-	}
-}
