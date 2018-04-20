051a92f861ee66e2bb30ab55e6c5b4fb27a69562
lzt <271105196@qq.com>
2018-04-20 18:08:38 +0800

    ss

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -4,7 +4,6 @@ use models\User;
 class IndexController extends Controller {
 	public function indexAction() {//默认Action
 		$data = User::get();
-		header( "HTTP/1.1 502 Not Found" );
 		$this->display('index',['data'=>$data]);
 	}
 }
