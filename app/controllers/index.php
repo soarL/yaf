4b957b54d4daebcd21db75da1e36c0c92d8865e4
lzt <271105196@qq.com>
2018-04-20 14:26:01 +0800

    ss

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -1,10 +1,9 @@
 <?php
-use models\User;
-use Illuminate\Database\Capsule\Manager as DB;
+// use models\User;
+// use Illuminate\Database\Capsule\Manager as DB;
 
 class IndexController extends Controller {
 	public function indexAction() {//默认Action
-		$user = User::get();
-		$this->display('index',['data'=>$user]);
+		$this->display('index');
 	}
 }
